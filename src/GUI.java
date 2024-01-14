dfa64e1e404aae6a123fb57f2cb59b27c576e590
pratham <prathamnilange@gmail.com>
2024-01-14 20:20:34 +0530

    added panel

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,25 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame{
     GUI(){
-        setSize(500, 500);
+        setSize(800, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
-        //addComponents();
+        addComponents();
+    }
+    private static void addComponents(){
+        JPanel panel = new JPanel();
+        panel.setSize(800, 800);
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(null);
+
+
+        
+        GUI h = new GUI();
+        h.add(panel);
     }
 }
