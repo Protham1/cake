ee9c9317be3ed15da16036929cbe73c6b0e212bb
pratham <prathamnilange@gmail.com>
2024-01-14 20:13:35 +0530

    added main

---
+++ src/GUI.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class GUI extends JFrame{
+    GUI(){
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        //addComponents();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        GUI g = new GUI();
+        g.setVisible(true);
+    }
+}
