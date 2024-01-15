9978217f9f2e7cf57c3114129154784095608278
pratham <prathamnilange@gmail.com>
2024-01-15 08:26:55 +0530

    added header

--- src/GUI.java
+++ src/GUI.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
@@ -25,19 +26,29 @@ public class GUI extends JFrame{
        JTextArea morseTextArea, textArea;
 
 
+       textArea = new JTextArea();
+       textArea.setBounds(30, 120, 720, 350);
+       textArea.setFont(new Font("Dialog", Font.BOLD, 24));
+       textArea.setBorder(new LineBorder(Color.black, 3));
+       textArea.setForeground(Color.darkGray);
+
+
        morseTextArea = new JTextArea();
-       morseTextArea.setBounds(30, 120, 720, 350);
+       morseTextArea.setBounds(30, 520, 720, 350);
        morseTextArea.setFont(new Font("Dialog", Font.BOLD, 24));
        morseTextArea.setBorder(new LineBorder(Color.black, 3));
        morseTextArea.setForeground(Color.darkGray);
 
-
-       textArea = new JTextArea();
-       textArea.setBounds(30, 520, 720, 350);
+       JLabel Header = new JLabel("MORSE CODE TRANSLATOR");
+       Header.setBounds(30, 10, 720, 100);
+       Header.setHorizontalAlignment(JLabel.CENTER);
+       Header.setForeground(Color.WHITE);
+       Header.setFont(new Font("Dialog", Font.BOLD, 20));
 
 
        panel.add(morseTextArea);
        panel.add(textArea);
+       panel.add(Header);
 
 
 
